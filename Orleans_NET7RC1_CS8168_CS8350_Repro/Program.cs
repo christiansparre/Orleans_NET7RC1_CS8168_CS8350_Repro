7599b42886a910ab4a05bf00785009edb33e833f
Christian Sparre <christian@sparre.it>
2022-09-19 11:53:11 +0200

    Add project files.

---
+++ Orleans_NET7RC1_CS8168_CS8350_Repro/Program.cs
@@ -0,0 +1,8 @@
+﻿using Orleans.Serialization.Buffers;
+using Orleans.Serialization.GeneratedCodeHelpers;
+using Orleans.Serialization.WireProtocol;
+
+var reader = new Reader<int>();
+var field = new Field();
+
+OrleansGeneratedCodeHelper.ReadHeaderExpectingEndBaseOrEndObject(ref reader, ref field, 1);
